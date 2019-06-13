e49a253d608f35c425dda9fee7784aa9cc6a6521
gisellehargrove <gisellehargrove@gmail.com>
2019-06-13 17:05:27 -0400

    completed Students class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,18 +11,46 @@ class Person {
   }
 }
 
-class Students extends Person{
-  constructor(attrs) {
-    super(attrs);
-  }
-}
-
 class Instructors extends Person {
   constructor(attrs) {
     super(attrs);
+
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+
+  demo(subj) {
+    return `Today we are learning about ${subj}`;
+  }
+
+  grade(studentObj, studentStr) {
+    `${studentObj.name} receives a perfect score on ${studentStr}`;
   }
 }
 
+class Students extends Person{
+  constructor(attrs) {
+    super(attrs);
+
+    this.previousBackground = attrs.previousBackground;
+    this.favSubjects = attrs.favSubjects;
+  }
+
+  listsSubjects() {
+    this.favSubjects.forEach((element) => {
+      console.log(element);
+    });
+  }
+
+  PRAssignment(subject) {
+    `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    `${this.namen} has begun sprint challenge on ${subject}`;
+  }
+}
 
 class ProjectManagers extends Instructors {
   constructor(attrs) {
