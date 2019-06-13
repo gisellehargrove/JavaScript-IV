cc9ac22b6462824e9b6ac7a34241b847517ae67f
gisellehargrove <gisellehargrove@gmail.com>
2019-06-13 16:47:25 -0400

    created Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,18 +1,31 @@
 // CODE here for your Lambda Classes
-class Instructors {
+class Person {
   constructor(attrs) {
+    this.name = attrs.name;
+    this.location = attrs.location;
+    this.age = attrs.age;
+  }
 
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
 
-class Students {
+class Students extends Person{
   constructor(attrs) {
-
+    super(attrs);
   }
 }
 
-class ProjectManagers {
+class Instructors extends Person {
   constructor(attrs) {
+    super(attrs);
+  }
+}
 
+
+class ProjectManagers extends Instructors {
+  constructor(attrs) {
+    super(attrs);
   }
 }
