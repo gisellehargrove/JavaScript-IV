add670612b2df933ff568b3339371d24291d5654
gisellehargrove <gisellehargrove@gmail.com>
2019-06-13 16:43:23 -0400

    creating Lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,18 @@
 // CODE here for your Lambda Classes
+class Instructors {
+  constructor(attrs) {
+
+  }
+}
+
+class Students {
+  constructor(attrs) {
+
+  }
+}
+
+class ProjectManagers {
+  constructor(attrs) {
+
+  }
+}
